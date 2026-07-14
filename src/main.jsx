66c8034681ebee8ad25f694ef97365a9551d0cc7
DendlySVK <mathewjapeson@gmail.com>
2026-07-15 00:08:10 +0200

    Refactor main.jsx to include Root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import LandingPage from "./LandingPage.jsx";
+
+function Root() {
+  const isApp = window.location.pathname.startsWith("/app");
+  return isApp ? <App /> : <LandingPage />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
